b485488f96866d49bc89156c47340afeecd3120d
rafael.colvara <rafael.colvara@gmail.com>
2024-05-01 09:04:08 -0300

    Criacao do card e wrap do thema em toda a aplicação

---
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -0,0 +1,47 @@
+import {ThemeProvider} from '@emotion/react'
+
+
+const tema = {
+    cores: {
+        branco: '',
+        atencao: '',
+        focus: '',
+        primarias: {
+            a: '#5754ED',
+            b: '',
+            c: ''
+        },
+        secundarias: {
+            a: '#EBEAF9',
+            b: '',
+            c: ''
+        },
+        neutras: {
+            a: '',
+            b: '',
+            c: ''
+        },
+        dark: {
+            a: '',
+            b: '',
+        }
+    },
+    espacamentos: {
+        xxs: '4px',
+        xs: '8px',
+        s: '16px',
+        m: '24px',
+        l: '32px',
+        xl: '48px',
+        xxl: '64px'
+    }
+}
+
+export const ProvedorTema = ({children}) => {
+
+
+    return <ThemeProvider theme={tema}>
+            {children}
+            </ThemeProvider>
+
+}
